9312ce71133a141f10f749e1880559f8f8dda844
Gusp26 <pandaho97@gmail.com>
2023-06-22 17:54:43 +0800

    VISION 3.3

--- app/src/main/java/com/example/cw2_227020684/ThenextActivity.kt
+++ app/src/main/java/com/example/cw2_227020684/ThenextActivity.kt
@@ -1,11 +1,23 @@
 package com.example.cw2_227020684
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ThenextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thenext)
+
+        val toolpage = findViewById<Button>(R.id.test1)
+
+        toolpage.setOnClickListener{
+            callActivity()
+        }
+    }
+
+    private fun callActivity() {
+        startActivity(Intent(this,ToolActivity::class.java))
     }
 }
