c72d3284edff04283ab01784f1a7b6fdc6f89915
Gusp26 <pandaho97@gmail.com>
2023-06-27 00:16:30 +0800

    VISION 3.11

--- app/src/main/java/com/example/cw2_227020684/HomeFragment.kt
+++ app/src/main/java/com/example/cw2_227020684/HomeFragment.kt
@@ -14,6 +14,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cw2_227020684.databinding.FragmentHomeBinding
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import java.io.ByteArrayOutputStream
 import java.lang.Exception
 import java.util.Base64
@@ -45,10 +46,44 @@ class HomeFragment : Fragment() {
         }
 
         binding.btnInsert.setOnClickListener(){
-
+            add_Data()
         }
 
         return root
+    }
+
+    private fun add_Data(){
+        val picName = binding.etName.text.toString()
+        val picDesc = binding.etDesc.text.toString()
+
+        if (picName.isEmpty()){
+            Toast.makeText(context,"Name is empty",Toast.LENGTH_SHORT).show()
+        }
+
+        if (picDesc.isEmpty()){
+            Toast.makeText(context,"Descrption is empty",Toast.LENGTH_SHORT).show()
+        }
+
+        if (sImage.equals("")){
+            Toast.makeText(context,"Image is empty or incorrect format",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            db = FirebaseDatabase.getInstance().getReference("Culture")
+            val culture = CultureDataClass(picName,picDesc,sImage)
+            val databaseReference = FirebaseDatabase.getInstance().reference
+            val id = databaseReference.push().key
+            db.child(id.toString()).setValue(culture).addOnSuccessListener {
+                binding.etName.text.clear()
+                binding.etDesc.text.clear()
+                sImage = ""
+                binding.imageView.setImageBitmap(null)
+                Toast.makeText(context,"Upload Successful",Toast.LENGTH_SHORT).show()
+
+            }.addOnFailureListener{
+                Toast.makeText(context,"Data insert failed",Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
     private val ActivityResultLauncher = registerForActivityResult<Intent,ActivityResult>(
