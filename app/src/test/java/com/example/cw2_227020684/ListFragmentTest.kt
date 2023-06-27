b88c1861a9bdef36b8c18dfb30726eb73f219ad0
Gusp26 <pandaho97@gmail.com>
2023-06-27 22:12:36 +0800

    VISION 6.2

---
+++ app/src/test/java/com/example/cw2_227020684/ListFragmentTest.kt
@@ -0,0 +1,16 @@
+package com.example.cw2_227020684
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class ListFragmentTest {
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun onCreateView() {
+    }
+}
